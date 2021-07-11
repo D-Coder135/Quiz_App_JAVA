834ec109064376d2b25fbcf90c5fe46920afdbae
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-11 14:12:36 +0530

    initialize the object for the Intent

--- app/src/main/java/com/example/quiz_app_java/ResultPage.java
+++ app/src/main/java/com/example/quiz_app_java/ResultPage.java
@@ -2,6 +2,7 @@ package com.example.quiz_app_java;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -23,7 +24,7 @@ public class ResultPage extends AppCompatActivity {
         startOver.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(ResultPage.this, MainActivity.class);
             }
         });
     }
