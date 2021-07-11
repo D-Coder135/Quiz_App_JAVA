8ad924f22a59a3ace4f665a18e4c55d9b7b48c88
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-11 13:41:00 +0530

    call the setOnClickListener() method along with the object nextButton

--- app/src/main/java/com/example/quiz_app_java/MainActivity.java
+++ app/src/main/java/com/example/quiz_app_java/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.quiz_app_java;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 
@@ -18,5 +19,12 @@ public class MainActivity extends AppCompatActivity {
         nextButton = findViewById(R.id.button);
         firstOption = findViewById(R.id.radioButton);
         secondOption = findViewById(R.id.radioButton2);
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
