0ce6f7deb50208cb399ba430ebedacf91d7964e9
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-11 13:37:37 +0530

    add an object for the Button palette

--- app/src/main/java/com/example/quiz_app_java/MainActivity.java
+++ app/src/main/java/com/example/quiz_app_java/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.quiz_app_java;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button nextButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
