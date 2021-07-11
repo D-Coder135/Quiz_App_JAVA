392efff4ce84ed73fefbc3ca90d9f36bdb93df57
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-11 13:39:30 +0530

    initialize the object nextButton

--- app/src/main/java/com/example/quiz_app_java/MainActivity.java
+++ app/src/main/java/com/example/quiz_app_java/MainActivity.java
@@ -15,5 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nextButton = findViewById(R.id.button);
+
     }
 }
