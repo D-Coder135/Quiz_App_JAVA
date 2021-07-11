ab673250b43c433fc138258573879f82d82e631b
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-11 13:38:19 +0530

    add objects for the RadioButton class

--- app/src/main/java/com/example/quiz_app_java/MainActivity.java
+++ app/src/main/java/com/example/quiz_app_java/MainActivity.java
@@ -4,10 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
     Button nextButton;
-
+    RadioButton firstOption, secondOption;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
