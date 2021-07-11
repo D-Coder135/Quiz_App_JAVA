abac340c2adabb2f7ffa0b673ecdcc8a0b568a1a
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-11 14:08:11 +0530

    initialize the object for the Intent class

--- app/src/main/java/com/example/quiz_app_java/ThirdPage.java
+++ app/src/main/java/com/example/quiz_app_java/ThirdPage.java
@@ -2,6 +2,7 @@ package com.example.quiz_app_java;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -29,6 +30,7 @@ public class ThirdPage extends AppCompatActivity {
                 } else {
                     --MainActivity.score;
                 }
+                Intent intent = new Intent(ThirdPage.this, ResultPage.class);
             }
         });
     }
