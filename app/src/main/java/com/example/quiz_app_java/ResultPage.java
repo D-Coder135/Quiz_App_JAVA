5f916059dd56d598f180e2fb2bb83a3433ed1644
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-11 13:31:09 +0530

    add an empty activity ResultPage inside the project

---
+++ app/src/main/java/com/example/quiz_app_java/ResultPage.java
@@ -0,0 +1,14 @@
+package com.example.quiz_app_java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResultPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result_page);
+    }
+}
